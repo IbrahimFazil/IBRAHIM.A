10aedaf9088960556e12915678c3cf5fe3a0e396
IbrahimFazil <ibrahim.a@lxisoft.com>
2024-01-11 15:07:37 +0530

    difficultCode

---
+++ difficultCode/Main.java
@@ -0,0 +1,43 @@
+import java.lang.String;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.io.IOException;
+  class Main extends Object  {
+    static boolean c;
+   
+     static  int z=123456789;
+    
+   public static final synchronized void  main(String[] args)throws IOException {
+      
+  PrintWriter out =new PrintWriter(System.out);
+        int s=(int)(Math.random()*5);
+        var iOexceptionHandling="";
+        
+        long y=s;
+        for(int i=-1;i<=+2-2;i=i+1){
+ System.out.println("System.out");{
+    int j=0;
+ }
+     int throwsexception;
+     boolean flag;
+     do{
+         flag=true;
+        
+         if(flag!=false){
+         System.out.println("import");
+         }
+         flag=false;
+     }while(flag==true&&1<=2);
+           continue;
+        }
+    }
+     static{
+         int i=5;
+         if(i<6){
+        boolean flag=c;
+         System.out.println(flag);
+     System.out.println(new String());
+         }
+    }
+        
+}
